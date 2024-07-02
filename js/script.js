10046aa33214ea030cceb5b09329ac27d6b9abcd
marco-scocca <marcoscocca1991@gmail.com>
2024-07-02 07:21:18 +0200

    multiple of 3 and 5

--- js/script.js
+++ js/script.js
@@ -7,3 +7,6 @@ for (let i = 1; i <= 100; i++) {
     else if (i % 5 == 0 && i % 3 != 0) {
         console.log('Buzz');
     }
+    else if (i % 3 == 0 && i % 5 != 0) {
+        console.log('FizzBuzz');
+    }
